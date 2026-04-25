653f4d64fca29fc01f58f666efce4978cc3511be
tykipanda <alejandro10londono@gmail.com>
2026-04-24 23:11:46 -0500

    se agrego export.ChatState en Archivo: src/types/chat.ts

--- src/types/chat.ts
+++ src/types/chat.ts
@@ -1,9 +1,14 @@
 export type MessageRole = 'user' | 'assistant';
 
-export interface Massage {
+export interface Message {
     id: string;
     role: MessageRole;
     content: string;
     timestamp: Date;
     isStreaming?: boolean; // true mientras llega la respuesta
+}
+
+export interface ChatState {
+    messages: Message[];
+    isLoading: boolean;
 }
