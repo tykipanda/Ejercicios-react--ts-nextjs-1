7ea5da1e3100073022ac2619421b9d5bd449dc64
tykipanda <alejandro10londono@gmail.com>
2026-04-24 23:03:49 -0500

    se agrego export.interface-Message en Archivo: src/types/chat.ts

--- src/types/chat.ts
+++ src/types/chat.ts
@@ -1,1 +1,9 @@
 export type MessageRole = 'user' | 'assistant';
+
+export interface Massage {
+    id: string;
+    role: MessageRole;
+    content: string;
+    timestamp: Date;
+    isStreaming?: boolean; // true mientras llega la respuesta
+}
