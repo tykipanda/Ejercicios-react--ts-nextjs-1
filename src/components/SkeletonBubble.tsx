520c8662812564582227af7308a3ddc93e2c1ab7
tykipanda <alejandro10londono@gmail.com>
2026-04-25 23:02:04 -0500

    prueba

--- src/components/SkeletonBubble.tsx
+++ src/components/SkeletonBubble.tsx
@@ -1,5 +1,8 @@
 export function SkeletonBubble () {
     return (
-        
+        <div className="flex items-start mb-3">
+            {/* Avatar skeleton */}
+            
+        </div>
     )
 }
