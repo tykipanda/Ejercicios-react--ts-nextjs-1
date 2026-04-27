37a4eeb899d9f0ac588c93aa283395b2ab668876
tykipanda <alejandro10londono@gmail.com>
2026-04-26 21:16:23 -0500

    se agrego return en src/components/ChatWindow.tsx

--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -18,3 +18,20 @@ export function ChatWindow({ messages, isLoading }: ChatWindowProps) {
   useEffect(() => {
     bottomRef.current?.scrollIntoView({ behavior: 'smooth' });
   }, [messages, isLoading]);
+
+  return(
+    <div className='flex-1 overflow-y-auto p-4'>
+      {messages.length === 0 && (
+        <div className="text-center text-gray-400 mt-20">
+          <p>Empieza una conversacion...</p>
+        </div>
+      )}
+
+      {messages.map ((msg) => (
+        <ChatBubble key={msg.id} message={msg} />
+      ))}
+
+      
+    </div>
+  );
+}
