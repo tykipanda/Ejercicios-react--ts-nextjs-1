85ddd61c14f724fd62f007454eef5881c7323e13
tykipanda <alejandro10londono@gmail.com>
2026-05-05 21:53:41 -0500

    se agrego return.span en src/components/MetricCard.tsx

--- src/components/MetricCard.tsx
+++ src/components/MetricCard.tsx
@@ -16,3 +16,7 @@ const colorMap = {
 export function MetricCard({ title, value, unit, icon, color }: MetricCardProps) {
   return (
     <div className={'p-4 rounded-xl border-2 ' + colorMap[color]}>
+      <div className="flex items-center justify-between mb-2">
+        <span className="text-2xl">{icon}</span>
+        <span className="text-xs font-medium opacity-70">{title}</span>
+      </div>
