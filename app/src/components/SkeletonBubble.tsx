5ddc9c1362bb39073a21e0aa91ea70f4d8eed997
tykipanda <alejandro10londono@gmail.com>
2026-05-11 22:43:17 -0500

    creación archivo y function SkeletonBubble

---
+++ app/src/components/SkeletonBubble.tsx
@@ -0,0 +1,5 @@
+export function SkeletonBubble() {
+  return (
+    <div className="flex items-start mb-3">
+      {/* Avatar skeleton */}
+      <div className="w-8 h-8 rounded-full bg-gray-200 animate-pulse mr-2" />
