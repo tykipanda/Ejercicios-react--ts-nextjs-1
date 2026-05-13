51f63018c03141d37112a0bb741f844cc0561df9
tykipanda <alejandro10londono@gmail.com>
2026-05-12 22:59:39 -0500

    creación imports

--- app/src/components/ChatWindow.tsx
+++ app/src/components/ChatWindow.tsx
@@ -1,1 +1,6 @@
 'use client';
+
+import { useEffect, useRef } from 'react';
+import { Message } from '@/types/chat';
+import { ChatBubble } from './ChatBubble';
+import { SkeletonBubble } from './SkeletonBubble';
