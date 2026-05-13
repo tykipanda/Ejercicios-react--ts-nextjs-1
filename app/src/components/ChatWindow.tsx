2672debcaa1e7b082e0fd02f49fff9b73fde9392
tykipanda <alejandro10londono@gmail.com>
2026-05-12 23:01:06 -0500

    creacion interface ChatWindowProps

--- app/src/components/ChatWindow.tsx
+++ app/src/components/ChatWindow.tsx
@@ -4,3 +4,8 @@ import { useEffect, useRef } from 'react';
 import { Message } from '@/types/chat';
 import { ChatBubble } from './ChatBubble';
 import { SkeletonBubble } from './SkeletonBubble';
+
+interface ChatWindowProps {
+  messages: Message[];
+  isLoading: boolean;
+}
