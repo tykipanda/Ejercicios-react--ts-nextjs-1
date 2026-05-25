acd3d8c600ac5ad7cdb280b754e19806c008028b
tykipanda <alejandro10londono@gmail.com>
2026-05-24 21:27:15 -0500

    export func LatencyChart

--- app/src/components/charts/LatencyChart.tsx
+++ app/src/components/charts/LatencyChart.tsx
@@ -5,3 +5,6 @@ import {
   Tooltip, ResponsiveContainer
 } from 'recharts';
 import { MetricPoint } from '@/types/metrics';
+
+export function LatencyChart({ data }: { data: MetricPoint[] }) {
+  const formatted = data.map(d => ({
